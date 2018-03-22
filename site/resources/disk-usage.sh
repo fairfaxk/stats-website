a104a0f76ba7253bb345d94e188d5a7fb962cae5
Ryan Roberge <roberger@wit.edu>
2018-03-21 22:14:55 -0400

    renamed stats file to .csv

--- site/resources/disk-usage.sh
+++ site/resources/disk-usage.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo "DATE,USAGE,TOTAL" > disk-usage.txt
+echo "DATE,USAGE,TOTAL" > disk-usage.csv
 
 while [ 1 -eq 1 ]
 do
 
-echo -n `date | awk '{print $4}'`"," >> disk-usage.txt
-echo -n $(df -BM | grep '/$' | awk '{print $3}' | sed 's/[^0-9]*//g') >> disk-usage.txt && echo -n ',' >> disk-usage.txt && echo $(df -BM | grep '/$' | awk '{print $2}' | sed 's/[^0-9]*//g') >> disk-usage.txt
+echo -n `date | awk '{print $4}'`"," >> disk-usage.csv
+echo -n $(df -BM | grep '/$' | awk '{print $3}' | sed 's/[^0-9]*//g') >> disk-usage.csv && echo -n ',' >> disk-usage.csv && echo $(df -BM | grep '/$' | awk '{print $2}' | sed 's/[^0-9]*//g') >> disk-usage.csv
 
 
-if [ "$(cat disk-usage.txt | wc -l)" == "62" ]
+if [ "$(cat disk-usage.csv | wc -l)" == "62" ]
 then
-sed -i -e '2d' disk-usage.txt
+sed -i -e '2d' disk-usage.csv
 fi
 
 sleep 1
