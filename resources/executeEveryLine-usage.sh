ae1ec87bb61b95af0cb8712d659ba1a1da98865b
Timothy Boss <bosst@wit.edu>
2018-03-23 15:52:34 -0400

    update

--- resources/executeEveryLine-usage.sh
+++ resources/executeEveryLine-usage.sh
@@ -1,53 +1,23 @@
 #!/bin/bash
-
-
-
 # USAGE:
-
 # ./executeEveryLine customCommandsFile customCommandsOutputFile &
-
-
 
 newline=`echo`
 
-
-
 while [ 1 -eq 1 ]
-
 do
-
-
-
- echo > $2
-
-
-
  while read line;
-
  do
-
          if [ "$line" != "$newline" ]
-
          then
 
-                 echo "$line:" >> $2
+                 echo "Custom Command: $line" >> $2
 
-                 echo "====================" >> $2
-
-                 eval "$line" &>> $2
-
-                 echo >> $2
-
-                 echo >> $2
+                 eval "Output: $line" &>> $2
 
          fi
-
  done < "$1"
 
-
-
  sleep 1;
-
-
 
 done
