481018661651aebfbe5a7a1c5524df3d929f9fa1
Timothy Boss <bosst@wit.edu>
2018-03-24 00:47:16 -0400

    update

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 	<title>Dashboard</title>
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/d3/4.13.0/d3.js"></script>
 	<script type="text/javascript" src="js/initChart.js"></script>
 	<script type="text/javascript" src="js/cpuChart.js"></script>
