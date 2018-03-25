46b838ae36257d66ccdb7a5cfc3dc7d1f63bceee
Timothy Boss <bosst@wit.edu>
2018-03-25 19:27:40 -0400

    update

--- js/cpuChart.js
+++ js/cpuChart.js
@@ -1,4 +1,6 @@
 var ctx = document.getElementById("cpuChart").getContext('2d');
+ctx.canvas.width = 300;
+ctx.canvas.height = 300;
 var myChart = new Chart(ctx, {
     type: 'doughnut',
     data: {
@@ -16,8 +18,5 @@ var myChart = new Chart(ctx, {
             "Used",
             "Unused"
         ]
-    },
-    options: {
-        responsive: false
     }
 });
