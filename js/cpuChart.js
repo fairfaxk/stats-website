0d6baf02e4b8d01090b442940944b9b3d21f5f46
Timothy Boss <bosst@wit.edu>
2018-03-25 19:04:55 -0400

    update

--- js/cpuChart.js
+++ js/cpuChart.js
@@ -1,27 +1,29 @@
-var cpuCanvas = document.getElementById("cpu-chart");
+window.onload = function () {
+    var cpuCanvas = document.getElementById("cpu-chart");
 
-var cpuData = {
-    labels: [
-        "Saudi Arabia",
-        "Russia",
-        "Iraq",
-        "United Arab Emirates",
-        "Canada"
-    ],
-    datasets: [
-        {
-            data: [133.3, 86.2, 52.2, 51.2, 50.2],
-            backgroundColor: [
-                "#FF6384",
-                "#63FF84",
-                "#84FF63",
-                "#8463FF",
-                "#6384FF"
-            ]
-        }]
-};
+    var cpuData = {
+        labels: [
+            "Saudi Arabia",
+            "Russia",
+            "Iraq",
+            "United Arab Emirates",
+            "Canada"
+        ],
+        datasets: [
+            {
+                data: [133.3, 86.2, 52.2, 51.2, 50.2],
+                backgroundColor: [
+                    "#FF6384",
+                    "#63FF84",
+                    "#84FF63",
+                    "#8463FF",
+                    "#6384FF"
+                ]
+            }]
+    };
 
-var pieChart = new Chart(cpuCanvas, {
-  type: 'doughnut',
-  data: cpuData
-});
+    var pieChart = new Chart(cpuCanvas, {
+        type: 'doughnut',
+        data: cpuData
+    });
+}
