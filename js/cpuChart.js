cdef6b1958381c641ba141b4541b58f2fee19333
Timothy Boss <bosst@wit.edu>
2018-03-25 18:48:48 -0400

    update

--- js/cpuChart.js
+++ js/cpuChart.js
@@ -1,12 +1,14 @@
 new Chart(document.getElementById("cpu-chart"), {
-    type: 'pie',
+    type: 'doughnut',
     data: {
       labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
-      datasets: [{
-        label: "Population (millions)",
-        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-        data: [2478,5267,734,784,433]
-      }]
+      datasets: [
+        {
+          label: "Population (millions)",
+          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+          data: [2478,5267,734,784,433]
+        }
+      ]
     },
     options: {
       title: {
@@ -15,4 +17,3 @@ new Chart(document.getElementById("cpu-chart"), {
       }
     }
 });
-
