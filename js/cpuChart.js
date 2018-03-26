332af344f7ac372d29e97d28c4ba13a413b30f60
Timothy Boss <bosst@wit.edu>
2018-03-25 22:50:04 -0400

    update

--- js/cpuChart.js
+++ js/cpuChart.js
@@ -1,37 +1,38 @@
 function drawCPUChart() {
-    var data = d3.csv("resources/cpu-usage.csv", function (data) {
+    var dataUsed;
+    var dataUnused;
+    var csvData = d3.csv("resources/cpu-usage.csv", function (data) {
         data.forEach(function (d) {
             data.USAGE = +data.USAGE;
             //console.log(data);
-            return data;
+            dataUsed = data[60].USAGE;
+            dataUnused = 100 - data[60].USAGE;
         })
     })
-    console.log(data[60].USAGE);
-        var dataUsed = data[60].USAGE;
-        var dataUnused = 100 - data[60].USAGE;
-        var ctx = document.getElementById("cpuChart").getContext('2d');
-        var cpuChart = new Chart(ctx, {
-            type: 'pie',
-            data: {
-                datasets: [{
-                    data: [
-                        dataUsed,
-                        dataUnused
-                    ],
-                    backgroundColor: [
-                        "#007bff",
-                        "#C0C0C0"
-                    ]
-                }],
-                labels: [
-                    "% used",
-                    "% available"
+    console.log(dataUsed);
+    var ctx = document.getElementById("cpuChart").getContext('2d');
+    var cpuChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            datasets: [{
+                data: [
+                    dataUsed,
+                    dataUnused
+                ],
+                backgroundColor: [
+                    "#007bff",
+                    "#C0C0C0"
                 ]
-            },
-            options: {
-                legend: {
-                    display: false
-                }
+            }],
+            labels: [
+                "% used",
+                "% available"
+            ]
+        },
+        options: {
+            legend: {
+                display: false
             }
-        });
+        }
+    });
 }
