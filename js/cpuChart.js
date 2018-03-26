b4b741cc3be9cc85afa73001e464a002add4821b
Timothy Boss <bosst@wit.edu>
2018-03-25 20:18:01 -0400

    update

--- js/cpuChart.js
+++ js/cpuChart.js
@@ -1,5 +1,3 @@
-var dataUsed = null;
-var dataUnused = null;
 d3.csv("resources/cpu-usage.csv", function (data) {
     data.forEach(function (d) {
         data.USAGE = +data.USAGE;
@@ -7,31 +5,29 @@ d3.csv("resources/cpu-usage.csv", function (data) {
     })
     dataUsed = data[59].USAGE;
     dataUnused = 100 - data[59].USAGE;
-})
-console.log(dataUsed);
-console.log(dataUnused);
-var ctx = document.getElementById("cpuChart").getContext('2d');
-var cpuChart = new Chart(ctx, {
-    type: 'pie',
-    data: {
-        datasets: [{
-            data: [
-                1.68,
-                98.32
-            ],
-            backgroundColor: [
-                "#007bff",
-                "#C0C0C0"
+    var ctx = document.getElementById("cpuChart").getContext('2d');
+    var cpuChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            datasets: [{
+                data: [
+                    dataUsed,
+                    dataUnused
+                ],
+                backgroundColor: [
+                    "#007bff",
+                    "#C0C0C0"
+                ]
+            }],
+            labels: [
+                "% Used",
+                "% Available"
             ]
-        }],
-        labels: [
-            "% Used",
-            "% Available"
-        ]
-    },
-    options: {
-        legend: {
-            display: false
+        },
+        options: {
+            legend: {
+                display: false
+            }
         }
-    }
-});
+    });
+})
